ae638d7d8d5b175c98f6723b3fed3a3749b98416
osmanozysr <123673331+OsmanOzyasar@users.noreply.github.com>
2024-12-21 22:26:37 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Loops3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir sayı limiti giriniz: ");
+
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            int sayac_ = 0;
+
+            do
+            {
+                Console.WriteLine("Ben Patika'lıyım");
+                sayac_++;
+            } while (sayac_ <= userInput);
+
+            //while (sayac_ <= userInput)
+            //{
+            //    Console.WriteLine("Ben Patika'lıyım");
+            //    sayac_++;
+            //}
+
+            //do-While döngüsünde sayaçtan daha küçük bir değer girdiğimde bile sadece 1 kez "ben patika'lıyım" yazdırdı ama while döngüsünde koşul yanlış olduğu için hiç kodu başlatmadı. Bu da bize koşul yanlış olsa bile do-While döngüsünün bir kez çalıştığını gösteriyor
+        }
+    }
+}
